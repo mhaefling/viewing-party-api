300d9ab2ffd33813c147981f8a0dc085ca6c5e74
Matt Haefling <m.haefling@protonmail.com>
2025-02-05 21:55:55 -0700

    TDD: Movies by keyword, Movies by average vote

--- spec/requests/api/v1/movies_request_spec.rb
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Movies Endpoint" do
   describe "happy path" do
     it "can retrieve a list of movies currently playing in theaters", :vcr do
-      get '/api/v1/movies'
+      get "/api/v1/movies"
       expect(response).to be_successful
       
       json = JSON.parse(response.body, symbolize_names: true)[:data]
@@ -20,7 +20,7 @@ RSpec.describe "Movies Endpoint" do
     end
 
     it "can retrieve a list of the 20 top rated movies", :vcr do
-      get '/api/v1/movies?filter=top rated'
+      get "/api/v1/movies?filter=top rated"
       expect(response).to be_successful
       
       json = JSON.parse(response.body, symbolize_names: true)[:data]
@@ -33,6 +33,45 @@ RSpec.describe "Movies Endpoint" do
         expect(movie[:attributes][:title]).to be_an(String)
         expect(movie[:attributes]).to have_key(:vote_average)
         expect(movie[:attributes][:vote_average]).to be > 8
+        expect(movie[:attributes][:vote_average]).to be_an(Float)
+      end
+    end
+
+    it "can retrieve a list of movies by keyword", :vcr do
+      keyword1 = "the"
+      keyword2 = "lord"
+
+      get "/api/v1/movies?title=#{keyword1} #{keyword2}"
+      expect(response).to be_successful
+      
+      json = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(json.count).to eq(20)
+
+      json.each do |movie|
+        expect(movie[:id]).to be_an(String)
+        expect(movie[:type]).to eq("movie")
+        expect(movie[:attributes]).to have_key(:title)
+        expect(movie[:attributes][:title]).to be_an(String)
+        expect(movie[:attributes][:title].downcase).to include(keyword1, keyword2)
+        expect(movie[:attributes]).to have_key(:vote_average)
+        expect(movie[:attributes][:vote_average]).to be_an(Float)
+      end
+    end
+
+    it "can retrieve a list of movies by average vote", :vcr do
+      get "/api/v1/movies?average_votes=5.5"
+      expect(response).to be_successful
+      
+      json = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(json.count).to eq(20)
+
+      json.each do |movie|
+        expect(movie[:id]).to be_an(String)
+        expect(movie[:type]).to eq("movie")
+        expect(movie[:attributes]).to have_key(:title)
+        expect(movie[:attributes][:title]).to be_an(String)
+        expect(movie[:attributes]).to have_key(:vote_average)
+        expect(movie[:attributes][:vote_average]).to be >= 5.5
         expect(movie[:attributes][:vote_average]).to be_an(Float)
       end
     end
