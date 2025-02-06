70cd7e21b6f1172fc6eca7d23e7b4e0266a73b8d
Matt Haefling <m.haefling@protonmail.com>
2025-02-05 21:49:18 -0700

    TDD: Currently playing movies, 20 top rated movies

--- spec/requests/api/v1/movies_request_spec.rb
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -2,13 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Movies Endpoint" do
   describe "happy path" do
-    it "can retrieve a list of 20 most top rated movies", :vcr do
-        get '/api/v1/movies'
-
-        expect(response).to be_successful
-        json = JSON.parse(response.body, symbolize_names: true)[:data]
-
-
+    it "can retrieve a list of movies currently playing in theaters", :vcr do
+      get '/api/v1/movies'
+      expect(response).to be_successful
+      
+      json = JSON.parse(response.body, symbolize_names: true)[:data]
       expect(json.count).to eq(20)
 
       json.each do |movie|
@@ -17,6 +15,24 @@ RSpec.describe "Movies Endpoint" do
         expect(movie[:attributes]).to have_key(:title)
         expect(movie[:attributes][:title]).to be_an(String)
         expect(movie[:attributes]).to have_key(:vote_average)
+        expect(movie[:attributes][:vote_average]).to be_an(Float)
+      end
+    end
+
+    it "can retrieve a list of the 20 top rated movies", :vcr do
+      get '/api/v1/movies?filter=top rated'
+      expect(response).to be_successful
+      
+      json = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(json.count).to eq(20)
+
+      json.each do |movie|
+        expect(movie[:id]).to be_an(String)
+        expect(movie[:type]).to eq("movie")
+        expect(movie[:attributes]).to have_key(:title)
+        expect(movie[:attributes][:title]).to be_an(String)
+        expect(movie[:attributes]).to have_key(:vote_average)
+        expect(movie[:attributes][:vote_average]).to be > 8
         expect(movie[:attributes][:vote_average]).to be_an(Float)
       end
     end
