6b21021112215e2fb480351e5ba4c0b99d1fbe8c
Matt Haefling <m.haefling@protonmail.com>
2025-02-08 15:20:55 -0700

    Added additional error message for start time after end time sad path

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -8,6 +8,7 @@ class ViewingParty < ApplicationRecord
   has_many :user_viewings
   has_many :users, through: :user_viewings
 
+
   def self.create_party(host_user, invitees, party_details)
       host = User.find(host_user)
       new_party = host.viewing_parties.create!(party_details)
@@ -20,13 +21,15 @@ class ViewingParty < ApplicationRecord
   end
 
   def self.check_viewing_times(movie_details)
-    if movie_details[:movie_id] != nil
+    if movie_details[:movie_id] != nil && movie_details[:start_time] != nil && movie_details[:end_time] != nil
       movie_runtime = MovieGateway.movie_run_time(movie_details[:movie_id])
       start_time = movie_details[:start_time][11..-4].delete":"
       end_time = movie_details[:end_time][11..-4].delete":"
       party_length = end_time.to_i - start_time.to_i
-      if party_length < movie_runtime
-        raise StandardError, "Party length has to be longer than movies run time"
+      if movie_details[:start_time] >= movie_details[:end_time]
+        raise StandardError, "Your parties end time cannot be before its start_time"
+      elsif party_length < movie_runtime
+        raise StandardError, "The length of your party, must to be longer than the movies run time"
       end
     end
   end
