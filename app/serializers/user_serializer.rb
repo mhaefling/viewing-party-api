c9cfc9dfbe087e70f1ecc1eaea4af5a015ecaeb7
Matt Haefling <m.haefling@protonmail.com>
2025-02-10 14:30:52 -0700

    REFACTOR: Updated user serializer to output emptry arrays when no parties have been created

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -26,27 +26,35 @@ class UserSerializer
         name: user.name,
         username: user.username,
         viewing_parties_hosted: 
-        user.viewing_parties.joins(:user_viewings).where(user_viewings: { host: true }).distinct.map do |viewing|
-          {
-            id: viewing.id,
-            name: viewing.name,
-            start_time: viewing.start_time.to_s[0..18],
-            end_time: viewing.end_time.to_s[0..18],
-            movie_id: viewing.movie_id,
-            movie_title: viewing.movie_title,
-            host_id: user.id
-          }
+        if user.viewing_parties.joins(:user_viewings).where(user_viewings: { host: true }).distinct.empty?
+          []
+        else
+          user.viewing_parties.joins(:user_viewings).where(user_viewings: { host: true }).distinct.map do |viewing|
+            {
+              id: viewing.id,
+              name: viewing.name,
+              start_time: viewing.start_time.to_s[0..18],
+              end_time: viewing.end_time.to_s[0..18],
+              movie_id: viewing.movie_id,
+              movie_title: viewing.movie_title,
+              host_id: user.id
+            }
+          end
         end,
         viewing_parties_invited:
-        user.viewing_parties.joins(:user_viewings).where(user_viewings: { host: false }).distinct.map do |viewing|
-          {
-            name: viewing.name,
-            start_time: viewing.start_time.to_s[0..18],
-            end_time: viewing.end_time.to_s[0..18],
-            movie_id: viewing.movie_id,
-            movie_title: viewing.movie_title,
-            id: viewing.user_viewings.where(host: true)[0].user_id
-          }
+        if user.viewing_parties.joins(:user_viewings).where(user_viewings: { host: false }).distinct.empty?
+          []
+        else
+          user.viewing_parties.joins(:user_viewings).where(user_viewings: { host: false }).distinct.map do |viewing|
+            {
+              name: viewing.name,
+              start_time: viewing.start_time.to_s[0..18],
+              end_time: viewing.end_time.to_s[0..18],
+              movie_id: viewing.movie_id,
+              movie_title: viewing.movie_title,
+              id: viewing.user_viewings.where(host: true)[0].user_id
+            }
+          end
         end
       }
     }
